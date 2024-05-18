c03d8e4f9ce7918048c4087792e0befe0f2a9bd0
Pavel <pavel.en00@mail.ru>
2024-05-18 06:01:05 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using Jetpack_Joyride.Classes;
+using Jetpack_Joyride.Forms;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Jetpack_Joyride
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);            
+            Application.Run(new loading_form());                       
+        }
+    }
+}
